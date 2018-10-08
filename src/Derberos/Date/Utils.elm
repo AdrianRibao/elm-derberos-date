f03093bc8f33b0f9f32456c38729e3b562ca4188
Adrián Ribao <adrian@adrima.es>
2018-10-08 06:10:01 +0200

    improve some packages

--- src/Derberos/Date/Utils.elm
+++ src/Derberos/Date/Utils.elm
@@ -1,5 +1,19 @@
-module Derberos.Date.Utils exposing (a)
+module Derberos.Date.Utils exposing (hello)
+
+{-| Utils for working with dates.
 
 
+# Utils
+
+@docs hello
+
+-}
+
+import Time exposing (Posix)
+
+
+{-| This is just a simple hello world function
+-}
+hello : String
 hello =
     "world!"
