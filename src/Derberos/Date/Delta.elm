9684816398c856d598e69ca68b61a89361be32ac
Adrián Ribao Martínez <adrian@adrima.es>
2018-10-12 08:10:42 +0200

    Add years

--- src/Derberos/Date/Delta.elm
+++ src/Derberos/Date/Delta.elm
@@ -50,3 +50,35 @@ addDays delta time =
         |> posixToMillis
         |> (+) (delta * 1000 * 60 * 60 * 24)
         |> millisToPosix
+
+
+{-| Add years to the time
+-}
+addYears : Int -> Posix -> Posix
+addYears delta time =
+    let
+        _ =
+            Debug.log "Delta: " delta
+
+        millis =
+            posixToMillis time
+
+        yearMod =
+            modBy 4 2014
+
+        delta_years =
+            millis + (delta * 1000 * 60 * 60 * 24 * 365)
+
+        drift_4_years =
+            delta_years
+                + (((delta + yearMod) // 4) * 1000 * 60 * 60 * 24)
+
+        drift_100_years =
+            drift_4_years
+                - (((delta + yearMod) // 100) * 1000 * 60 * 60 * 24)
+
+        drift_400_years =
+            drift_100_years
+                + (((delta + yearMod) // 400) * 1000 * 60 * 60 * 24)
+    in
+    millisToPosix drift_400_years
--- tests/TestDelta.elm
+++ tests/TestDelta.elm
@@ -1,6 +1,6 @@
 module TestDelta exposing (..)
 
-import Derberos.Date.Delta exposing (addDays, addHours, addMinutes, addSeconds)
+import Derberos.Date.Delta exposing (addDays, addHours, addMinutes, addSeconds, addYears)
 import Expect
 import Test exposing (..)
 import Time exposing (Posix, millisToPosix)
@@ -20,7 +20,9 @@ all =
                             millisToPosix 1539005139000
                     in
                     Expect.equal (addSeconds 60 posixTime) expectedTime
-            , test "Add 60 minutes" <|
+            ]
+        , describe "Test add minutes"
+            [ test "Add 60 minutes" <|
                 \() ->
                     let
                         posixTime =
@@ -30,7 +32,9 @@ all =
                             millisToPosix 1539008679000
                     in
                     Expect.equal (addMinutes 60 posixTime) expectedTime
-            , test "Add 5 hours" <|
+            ]
+        , describe "Test add hours"
+            [ test "Add 5 hours" <|
                 \() ->
                     let
                         posixTime =
@@ -40,7 +44,9 @@ all =
                             millisToPosix 1539023079000
                     in
                     Expect.equal (addHours 5 posixTime) expectedTime
-            , test "Add 7 days" <|
+            ]
+        , describe "Test add days"
+            [ test "Add 7 days" <|
                 \() ->
                     let
                         posixTime =
@@ -50,5 +56,57 @@ all =
                             millisToPosix 1539609879000
                     in
                     Expect.equal (addDays 7 posixTime) expectedTime
+            ]
+        , describe "Test add years"
+            [ test "Add 1 years to 14/1/2014 (1393632000000)" <|
+                \() ->
+                    let
+                        posixTime =
+                            millisToPosix 1393632000000
+
+                        expectedTime =
+                            millisToPosix 1425168000000
+                    in
+                    Expect.equal (addYears 1 posixTime) expectedTime
+            , test "Add 2 years to 14/1/2014 (1393632000000)" <|
+                \() ->
+                    let
+                        posixTime =
+                            millisToPosix 1393632000000
+
+                        expectedTime =
+                            millisToPosix 1456790400000
+                    in
+                    Expect.equal (addYears 2 posixTime) expectedTime
+            , test "Add 3 years to 14/1/2014 (1393632000000)" <|
+                \() ->
+                    let
+                        posixTime =
+                            millisToPosix 1393632000000
+
+                        expectedTime =
+                            millisToPosix 1488326400000
+                    in
+                    Expect.equal (addYears 3 posixTime) expectedTime
+            , test "Add 200 years to 14/1/2014 (1393632000000)" <|
+                \() ->
+                    let
+                        posixTime =
+                            millisToPosix 1393632000000
+
+                        expectedTime =
+                            millisToPosix 7704979200000
+                    in
+                    Expect.equal (addYears 200 posixTime) expectedTime
+            , test "Add 500 years to 14/1/2014 (1393632000000)" <|
+                \() ->
+                    let
+                        posixTime =
+                            millisToPosix 1393632000000
+
+                        expectedTime =
+                            millisToPosix 17172086400000
+                    in
+                    Expect.equal (addYears 500 posixTime) expectedTime
             ]
         ]
